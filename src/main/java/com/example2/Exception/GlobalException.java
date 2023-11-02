cab43222edf2c3f72b2f41209d05dd2f5d24968c
venkatabujji <145656446+venkatabujji@users.noreply.github.com>
2023-11-02 10:57:37 +0530

    Update GlobalException.java

--- src/main/java/com/example2/Exception/GlobalException.java
+++ src/main/java/com/example2/Exception/GlobalException.java
@@ -1,7 +1,6 @@
 package com.example2.Exception;
 
 import java.util.Date;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -11,11 +10,9 @@ import org.springframework.web.context.request.WebRequest;
 public class GlobalException
 {
 	@ExceptionHandler(ResourceNotFoundException.class)
-	public ResponseEntity<?> resourceNotFoundExHandling(ResourceNotFoundException exception,
-			WebRequest request)
+	public ResponseEntity<?> resourceNotFoundExHandling(ResourceNotFoundException exception, WebRequest request)
 	{
-		ErrorDetails errorDetails=new ErrorDetails(new Date(), 
-				exception.getMessage(), request.getDescription(false));
+		ErrorDetails errorDetails=new ErrorDetails(new Date(), exception.getMessage(), request.getDescription(false));
 		return new ResponseEntity<>(errorDetails,HttpStatus.NOT_FOUND);
 	}
 }
