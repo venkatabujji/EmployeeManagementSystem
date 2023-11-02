2e2a063623e27ca83705ad3b478fb4408ef41eed
venkatabujji <145656446+venkatabujji@users.noreply.github.com>
2023-11-02 10:55:08 +0530

    Update JwtFilter.java

--- src/main/java/com/example2/config/JwtFilter.java
+++ src/main/java/com/example2/config/JwtFilter.java
@@ -1,17 +1,14 @@
 package com.example2.config;
 
 import java.io.IOException;
-
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 import org.springframework.http.HttpRequest;
 import org.springframework.web.filter.GenericFilterBean;
-
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureException;
@@ -20,8 +17,8 @@ public class JwtFilter extends GenericFilterBean
 {
 	@Override
 	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
-			throws IOException, ServletException {
-	
+	throws IOException, ServletException 
+	{
 		final HttpServletRequest request=(HttpServletRequest)req;
 		final HttpServletResponse response=(HttpServletResponse)res;
 		final String authHeader=request.getHeader("authorization");
@@ -38,10 +35,13 @@ public class JwtFilter extends GenericFilterBean
 			
 		}
 		final String token=authHeader.substring(7);
-		try {
+		try 
+		{
 		final Claims claims=Jwts.parser().setSigningKey("secretkey").parseClaimsJws(token).getBody();
 		request.setAttribute("claims", claims);
-		}catch (final SignatureException e) {
+		}
+		catch (final SignatureException e) 
+		{
 		throw new ServletException("Invalid token");
 		}
 		chain.doFilter(request, response);
