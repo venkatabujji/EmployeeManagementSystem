08df8362b949bc0a8066430231a6d2ae709a1c15
venkatabujji <145656446+venkatabujji@users.noreply.github.com>
2023-11-02 10:37:56 +0530

    Update Ems1Application.java

--- src/main/java/com/example2/Ems1Application.java
+++ src/main/java/com/example2/Ems1Application.java
@@ -4,25 +4,26 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
-
 import com.example2.config.JwtFilter;
 
 
 
 @SpringBootApplication
-public class Ems1Application {
-
+public class Ems1Application
+{
 	@SuppressWarnings("rawtypes")
 	@Bean
 	public FilterRegistrationBean jwtFilter()
 	{
 		final FilterRegistrationBean registrationBean=new FilterRegistrationBean();
-			registrationBean.setFilter(new JwtFilter());
-		   registrationBean.addUrlPatterns("/api/*");
-			return registrationBean;
+	        registrationBean.setFilter(new JwtFilter());
+		registrationBean.addUrlPatterns("/api/*");
+		return registrationBean;
 	
 	}
-	public static void main(String[] args) {
+	
+	public static void main(String[] args)
+	{
 		SpringApplication.run(Ems1Application.class, args);
 	}
 
