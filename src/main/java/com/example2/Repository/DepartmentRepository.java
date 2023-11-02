8cf175f6b740e0739903ba8be39bc9330f3269d2
venkatabujji <145656446+venkatabujji@users.noreply.github.com>
2023-11-02 10:59:26 +0530

    Update DepartmentRepository.java

--- src/main/java/com/example2/Repository/DepartmentRepository.java
+++ src/main/java/com/example2/Repository/DepartmentRepository.java
@@ -2,7 +2,6 @@ package com.example2.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import com.example2.Entity.Department;
 
 
