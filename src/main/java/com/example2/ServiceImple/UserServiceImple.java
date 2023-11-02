b65e5d34fb58e6fd05321a11a0dcbee916e23887
venkatabujji <145656446+venkatabujji@users.noreply.github.com>
2023-11-02 10:45:26 +0530

    Update UserServiceImple.java

--- src/main/java/com/example2/ServiceImple/UserServiceImple.java
+++ src/main/java/com/example2/ServiceImple/UserServiceImple.java
@@ -1,18 +1,12 @@
 package com.example2.ServiceImple;
 
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import com.example2.Entity.User;
 import com.example2.Repository.UserRepository;
 import com.example2.Service.UserService;
 import com.example2.Util.Converter;
-
-
-
-
 
 @Service
 public class  UserServiceImple implements UserService
@@ -23,16 +17,12 @@ public class  UserServiceImple implements UserService
 	@Autowired
 	private Converter converter;
 	
-	
-	
 	@Override
 	public User login(String userName, String userPassword) 
 	{
 		 User user=userRepository.findByUserNameAndPassword(userName, userPassword);
 		 return user;
 	}
-
-
 
 	@Override
 	public List<User> getAllusers() {
